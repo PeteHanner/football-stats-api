1f27554cb48b9d3b89c9b02396a79dbf86254fbc
Pete Hanner <petehanner@gmail.com>
2020-12-06 17:48:12 -0500

    WIP game stats worker

--- app/workers/game_import_worker.rb
+++ app/workers/game_import_worker.rb
@@ -1,7 +1,8 @@
 require "httparty"
 
 class GameImportWorker
-  include Sidekiq::GameImportWorker
+  include Sidekiq::Worker
+
   sidekiq_options retry: false
 
   # 401265703
@@ -9,36 +10,50 @@ class GameImportWorker
   def perform(season = nil, week = nil)
     if season.nil? || week.nil?
       last_game = Game.last
-      season = last_game.season
-      week = last_game.week
+      season, week = last_game.season, last_game.week
     end
 
-    response = HTTParty.get("https://api.collegefootballdata.com/games?year=#{season}&week=#{week}")
+    query_string = "https://api.collegefootballdata.com/games?year=#{season}&week=#{week}"
+    response = HTTParty.get(query_string)
 
     return unless response.code == 200
 
-    # data = JSON.parse(response.body)[0]
-    # binding.pry
-
-    # return unless data["home_points"].present?
-
-    game = Game.new(
-      api_id: data["id"],
-      season: data["season"],
-      home_team: data["home_team"],
-      home_team_score: data["home_points"],
-      away_team: data["away_team"],
-      away_team_score: data["away_points"],
-    )
-    # game.get_drive_data(season: data.season, week: data.week, team: data.home_team)
-    # game.generate_stats
-    # Game.load_new_game(game.api_id) if game.save
+    games_data = JSON.parse(response.body)
+    games_data.each do |game_data|
+      create_game(game_data)
+    end
   end
 
   private
 
-  def get_drive_data(season:, week:, team:)
-    # https://api.collegefootballdata.com/drives?year=2019&week=1&team=Auburn
+  def create_game(game_data)
+    return unless game_data["home_points"].present?
+    return if Game.find_by(api_id: game_data["id"]).present?
+
+    query_string = "https://api.collegefootballdata.com/drives?year=#{game_data["season"]}&week=#{game_data["week"]}&team=#{game_data["home_team"]}"
+    response = HTTParty.get(query_string)
+
+    return unless response.code == 200
+
+    binding.pry
+    game = Game.new(
+      api_id: game_data["id"],
+      away_team: game_data["away_team"],
+      away_team_score: game_data["away_points"],
+      home_team: game_data["home_team"],
+      home_team_score: game_data["home_points"],
+      season: game_data["season"],
+      week: game_data["week"]
+    )
+
+    drives = JSON.parse(response.body)
+    game.home_team_drives, game.away_team_drives = get_drive_breakdown(drive_data: drives, home_team: game_data["home_team"])
+
+    # game.generate_stats
+    # Game.load_new_game(game.api_id) if game.save
+  end
+
+  def get_drive_breakdown(drive_data:, home_team:)
     # Count up offensive drives per team
   end
 end
