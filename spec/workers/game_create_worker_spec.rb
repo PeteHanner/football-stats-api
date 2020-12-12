16f9915de3d3dba72c6ab3f718f9930e552dfb59
Pete Hanner <petehanner@gmail.com>
2020-12-11 19:16:25 -0500

    WIP specs for worker

--- spec/workers/game_create_worker_spec.rb
+++ spec/workers/game_create_worker_spec.rb
@@ -1,4 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
+
 RSpec.describe GameCreateWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform" do
+    it "returns if the game score is not yet present" do
+      incomplete_data = {"foo": "bar"}
+
+      GameCreateWorker.new.perform(incomplete_data)
+      expect(Game.all.count).to eq(0)
+    end
+
+    # it "will not create a duplicate game"
+
+    # it "returns without failing if the CFB API cannot be accessed" do
+    #   response = {code: 500}
+    #   allow(HTTParty).to receive(:get).and_return(response)
+    # end
+
+    it "creates a game object from a successfully returned API call" do
+      response = JSON.parse(File.read("spec/factories/game_api_response.json"))
+    end
+  end
 end
