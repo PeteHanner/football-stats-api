d989ad52941d92e4b066ef891573a419a8610990
Pete Hanner <petehanner@gmail.com>
2020-12-12 10:18:43 -0500

    Add golden path test for GameImportWorker

--- app/workers/game_import_worker.rb
+++ app/workers/game_import_worker.rb
@@ -5,27 +5,34 @@ class GameImportWorker
   sidekiq_options retry: true, unique: :until_executed
 
   def perform(season = nil, week = nil)
-    # Unless specified, continue getting new games from same week as last successfully imported game
-    # Also begin checking for new games from week following last successfully imported game
-    # Also check if new season has begun and begin importing from there if applicable
-    if season.nil? || week.nil?
-      last_game = Game.last
-      season, week = last_game.season, last_game.week
-      GameImportWorker.perform_async(season, week + 1)
-      GameImportWorker.perform_async(season + 1, 1)
-    end
+    @season, @week = season, week
+    check_current_and_next_weeks
 
-    query_string = "https://api.collegefootballdata.com/games?year=#{season}&week=#{week}"
+    query_string = "https://api.collegefootballdata.com/games?year=#{@season}&week=#{@week}"
     response = HTTParty.get(query_string)
 
     unless response.code == 200
-      Rails.logger.error "Game data request for #{season} season week #{week} returned response code #{response.code}"
+      Rails.logger.error "Game data request for #{@season} season week #{@week} returned response code #{response.code}"
       return false
     end
 
     games_data = JSON.parse(response.body)
     games_data.each do |game_data|
       GameCreateWorker.perform_async(game_data)
+    end
+  end
+
+  private
+
+  def check_current_and_next_weeks
+    # Unless specified, continue getting new games from same week as last successfully imported game
+    # Also begin checking for new games from week following last successfully imported game
+    # Also check if new season has begun and begin importing from there if applicable
+    if @season.nil? || @week.nil?
+      last_game = Game.last
+      @season, @week = last_game.season, last_game.week
+      GameImportWorker.perform_async(season, week + 1)
+      GameImportWorker.perform_async(season + 1, 1)
     end
   end
 end
--- spec/workers/game_import_worker_spec.rb
+++ spec/workers/game_import_worker_spec.rb
@@ -1,4 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 RSpec.describe GameImportWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform" do
+    it "calls GameCreateWorker on each game returned from the API query" do
+      games = File.read("spec/factories/games_api_response.json")
+      response = OpenStruct.new({code: 200, body: games})
+      allow(HTTParty).to receive(:get).and_return(response)
+      number_of_games = JSON.parse(games).length
+
+      expect(GameCreateWorker).to receive(:perform_async).exactly(number_of_games).times
+
+      GameImportWorker.new.perform(0, 0)
+    end
+
+    it "also checks next week + first week of next season if no arguments provided"
+
+    it "logs error and safely exits if API query fails"
+  end
 end
