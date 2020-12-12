543ab2a06b68b3e4e445295878e8f5e8e541a7a8
Pete Hanner <petehanner@gmail.com>
2020-12-12 14:06:01 -0500

    Add APOP methods and specs

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,11 +2,30 @@ class Team < ApplicationRecord
   has_many :stats
   has_many :games, through: :stats
 
-  def pop_over_season(season)
-    stats.where(name: "pop", season: season)
+  def apop(season, overwrite = false)
+    # Average Points per Offensive Possession
+    if overwrite
+      Rails.cache.fetch("#{name}/APOP/#{season}", force: true, expires_in: 1.day) do
+        calculate_apop(season)
+      end
+    else
+      Rails.cache.fetch("#{name}/APOP/#{season}", expires_in: 1.day) do
+        calculate_apop(season)
+      end
+    end
+  end
+
+  def calculate_apop(season)
+    total_pop = pop_over_season(season).pluck(:value).sum
+    games_played = pop_over_season(season).count
+    total_pop / games_played
   end
 
   def pdp_over_season(season)
     stats.where(name: "pdp", season: season)
+  end
+
+  def pop_over_season(season)
+    stats.where(name: "pop", season: season)
   end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,15 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  describe "#pop_over_season" do
-    it "returns an array of a team's POP for only the season provided" do
-      team = create(:team)
-      create(:stat, team: team, name: "pop", season: 2000)
-      create(:stat, team: team, name: "pop", season: 2000)
-      excluded_stat = create(:stat, team: team, name: "pop", season: 2001)
+  describe "#apop" do
+    let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+    let(:cache) { Rails.cache }
 
-      expect(team.pop_over_season(2000).length).to eq(2)
-      expect(team.pop_over_season(2000)).not_to include(excluded_stat)
+    before do
+      allow(Rails).to receive(:cache).and_return(memory_store)
+      Rails.cache.clear
+    end
+
+    it "writes to the cache if overwrite is specified" do
+      team = create(:team)
+      create(:stat, team: team, name: "pop", season: 2000, value: 5)
+      create(:stat, team: team, name: "pop", season: 2000, value: 3)
+
+      team.apop(2000, true)
+
+      expect(Rails.cache.read("#{team.name}/APOP/2000")).to eq(4)
+
+      create(:stat, team: team, name: "pop", season: 2000, value: 1)
+      team.apop(2000, true)
+
+      expect(Rails.cache.read("#{team.name}/APOP/2000")).to eq(3)
+    end
+
+    it "writes to the cache if no key exists" do
+      team = create(:team)
+      create(:stat, team: team, name: "pop", season: 2000, value: 5)
+      create(:stat, team: team, name: "pop", season: 2000, value: 3)
+
+      expect(Rails.cache.read("#{team.name}/APOP/2000")).to eq(nil)
+
+      team.apop(2000, true)
+
+      expect(Rails.cache.read("#{team.name}/APOP/2000")).to eq(4)
+    end
+
+    it "does not write to the cache if the key exists & not forced" do
+      team = create(:team)
+      create(:stat, team: team, name: "pop", season: 2000, value: 5)
+      create(:stat, team: team, name: "pop", season: 2000, value: 3)
+
+      team.apop(2000, true)
+
+      expect(Rails.cache.read("#{team.name}/APOP/2000")).to eq(4)
+
+      create(:stat, team: team, name: "pop", season: 2000, value: 1)
+      team.apop(2000)
+
+      expect(Rails.cache.read("#{team.name}/APOP/2000")).to eq(4)
+    end
+  end
+
+  describe "#calculate_apop" do
+    it "averages the team's POP across a season" do
+      team = create(:team)
+      create(:stat, team: team, name: "pop", season: 2000, value: 5)
+      create(:stat, team: team, name: "pop", season: 2000, value: 3)
+
+      expect(team.calculate_apop(2000)).to eq(4)
     end
   end
 
@@ -22,6 +72,18 @@ RSpec.describe Team, type: :model do
 
       expect(team.pdp_over_season(2000).length).to eq(2)
       expect(team.pdp_over_season(2000)).not_to include(excluded_stat)
+    end
+  end
+
+  describe "#pop_over_season" do
+    it "returns an array of a team's POP for only the season provided" do
+      team = create(:team)
+      create(:stat, team: team, name: "pop", season: 2000)
+      create(:stat, team: team, name: "pop", season: 2000)
+      excluded_stat = create(:stat, team: team, name: "pop", season: 2001)
+
+      expect(team.pop_over_season(2000).length).to eq(2)
+      expect(team.pop_over_season(2000)).not_to include(excluded_stat)
     end
   end
 end
