e891911ff25d918824f8dc3be2f83c42fd257293
Pete Hanner <petehanner@gmail.com>
2020-12-13 19:35:28 -0500

    Spec out main worker, create file for sub worker

---
+++ spec/workers/second_order_game_stats_calculate_worker_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+RSpec.describe SecondOrderGameStatsCalculateWorker, type: :worker do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
--- spec/workers/second_order_game_stats_worker_spec.rb
+++ spec/workers/second_order_game_stats_worker_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 RSpec.describe SecondOrderGameStatsWorker, type: :worker do
   describe "#perform" do
-    it "logs error and returns safely if team not found"
+    it "logs error and returns safely if team not found" do
+      bad_id = Team.all.count + 1
+
+      expect(Rails.logger).to receive(:error).with("SecondOrderGameStatsWorker unable to find team of ID #{bad_id}")
+
+      SecondOrderGameStatsWorker.new.perform(2000, bad_id)
+    end
+
+    it "calls SecondOrderGameStatsCalculateWorker async for each game in a team's season" do
+      team = create(:team)
+      create_list(:stat, 3, team: team, game: create(:game, season: 2000))
+      create(:stat, team: team, game: create(:game, season: 2001))
+
+      expect(SecondOrderGameStatsCalculateWorker).to receive(:perform_async).exactly(3).times
+
+      SecondOrderGameStatsWorker.new.perform(2000, team.id)
+    end
   end
 end
