fc7001a292a24cc09415ed0e6e8c61301b6265d6
Pete Hanner <petehanner@gmail.com>
2020-12-19 11:48:44 -0500

    Use Faker for factories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -2,20 +2,20 @@ FactoryBot.define do
   factory :game do
     api_ref { 401110723 }
     away_team_drives { 29 }
-    away_team_name { "Miami" }
+    away_team_name { Faker::University.name }
     away_team_score { 20 }
     home_team_drives { 0 }
-    home_team_name { "Florida" }
+    home_team_name { Faker::University.name }
     home_team_score { 24 }
     season { 2019 }
     week { 1 }
   end
 
   factory :team do
-    name { "Test University" }
+    name { Faker::University.name }
 
     trait :opponent do
-      name { "Test State" }
+      name { Faker::University.name }
     end
   end
 
