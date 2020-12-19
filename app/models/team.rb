8c5765ffcb404242082996c72c05f80aa93c8561
Pete Hanner <petehanner@gmail.com>
2020-12-19 10:44:51 -0500

    Model DPR after existing cached stats

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,11 +2,20 @@ class Team < ApplicationRecord
   has_many :stats
   has_many :games, through: :stats
 
+  def adpr(season:, overwrite: false)
+    # Average Defensive Performance Ratio
+    # Sum of all DPR scores ÷ games played
+    # By what percentage is your defense usually better than all your opponents' faced defenses?
+    Rails.cache.fetch("#{name.parameterize}/aopr/#{season}", force: overwrite, expires_in: 6.hours) do
+      calculate_adpr(season)
+    end
+  end
+
   def apdp(season:, overwrite: false)
     # Average Points per Defensive Possession
     # Sum of all PDP scores ÷ games played
     # How good is your defense on average?
-    Rails.cache.fetch("#{name.parameterize}/apdp/#{season}", force: overwrite, expires_in: 1.day) do
+    Rails.cache.fetch("#{name.parameterize}/apdp/#{season}", force: overwrite, expires_in: 6.hours) do
       calculate_apdp(season)
     end
   end
@@ -15,9 +24,15 @@ class Team < ApplicationRecord
     # Average Points per Offensive Possession
     # Sum of all POP scores ÷ games played
     # How good is your offense on average?
-    Rails.cache.fetch("#{name.parameterize}/apop/#{season}", force: overwrite, expires_in: 1.day) do
+    Rails.cache.fetch("#{name.parameterize}/apop/#{season}", force: overwrite, expires_in: 6.hours) do
       calculate_apop(season)
     end
+  end
+
+  def calculate_adpr(season)
+    total_dpr = dpr_over_season(season).pluck(:value).sum
+    games_played = dpr_over_season(season).count
+    total_dpr / games_played
   end
 
   def calculate_apdp(season)
@@ -30,6 +45,10 @@ class Team < ApplicationRecord
     total_pop = pop_over_season(season).pluck(:value).sum
     games_played = pop_over_season(season).count
     total_pop / games_played
+  end
+
+  def dpr_over_season(season)
+    stats.where(name: "dpr", season: season)
   end
 
   def pdp_over_season(season)
