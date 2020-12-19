d1e9190f4fa7dc051cec8eb8e7f5e1bb25c553a9
Pete Hanner <petehanner@gmail.com>
2020-12-19 11:36:24 -0500

    Fix typo

--- app/workers/second_order_game_stats_calculate_worker.rb
+++ app/workers/second_order_game_stats_calculate_worker.rb
@@ -12,14 +12,14 @@ class SecondOrderGameStatsCalculateWorker
 
   private
 
-  def caclculate_dpr_value
+  def calculate_dpr_value
     # Defensive Performance Ratio
     # By what percentage was your defense better/worse than the others your opponent has faced on the season?
     pdp = Stat.find_by(name: "pdp", game_id: @game.id, team_id: @team.id).value
     100.0 * (@opponent.apop(season: @game.season) / pdp)
   end
 
-  def caclculate_opr_value
+  def calculate_opr_value
     # Offensive Performance Ratio
     # By what percentage was your offense better/worse than the others your opponent has faced on the season?
     pop = Stat.find_by(name: "pop", game_id: @game.id, team_id: @team.id).value
@@ -52,8 +52,8 @@ class SecondOrderGameStatsCalculateWorker
   def write_or_overwrite_stats
     opr = set_opr_object
     dpr = set_dpr_object
-    opr.value = caclculate_opr_value
-    dpr.value = caclculate_dpr_value
+    opr.value = calculate_opr_value
+    dpr.value = calculate_dpr_value
 
     begin
       opr.save!
