0166a79cbcd9851d07b6a8e55fb4c3a4fd4c4f27
Pete Hanner <petehanner@gmail.com>
2020-12-20 17:25:10 -0500

    Set up routes and controller actions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,16 @@
 class TeamsController < ApplicationController
+  def show
+    @season = params[:season].to_i
+    return redirect_to season_path(Game.current_season) if bad_params?
+
+    h = {season: @season}
+
+    render json: h.to_json
+  end
+
+  private
+
+  def bad_params?
+    params[:season].blank? || Game.find_by(season: params[:season]).blank?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "/seasons/:id", to: "teams#show"
+  get "/seasons/:season", to: "teams#show", as: "season"
+  get "/seasons/", to: "teams#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
