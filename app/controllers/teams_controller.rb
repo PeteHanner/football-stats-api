4aa8aa2783dd2339d05aae2c154f62bb01e5a8e8
Pete Hanner <petehanner@gmail.com>
2020-12-20 20:15:51 -0500

    Use filter method in controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,7 +3,7 @@ class TeamsController < ApplicationController
     season = params[:season].to_i
     return redirect_to season_path(Game.current_season) if bad_params?
 
-    team_stats = Team.all
+    team_stats = Team.all_with_games_in_season(season)
 
     render json: team_stats, each_serializer: TeamSerializer, season: season
   end
