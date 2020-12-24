15e8f1bea721b5d18ec5e2696e71406e9ff2b3b7
Pete Hanner <petehanner@gmail.com>
2020-12-23 22:17:29 -0500

    Refactor first order game stats

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,10 +5,10 @@ class Game < ApplicationRecord
   validates_presence_of :api_ref
 
   def home_team
-    Team.find_by(name: home_team_name)
+    Team.find_or_create_by(name: home_team_name)
   end
 
   def away_team
-    Team.find_by(name: away_team_name)
+    Team.find_or_create_by(name: away_team_name)
   end
 end
--- app/workers/first_order_game_stats_worker.rb
+++ app/workers/first_order_game_stats_worker.rb
@@ -7,8 +7,8 @@ class FirstOrderGameStatsWorker
 
     raise "ERROR: #{self.class.name} unable to find Game ID #{game_id}" if @game.nil?
 
-    @home_team = Team.find_or_create_by(name: @game.home_team_name)
-    @away_team = Team.find_or_create_by(name: @game.away_team_name)
+    @home_team = @game.home_team
+    @away_team = @game.away_team
 
     set_pop_and_pdp
 
@@ -17,45 +17,60 @@ class FirstOrderGameStatsWorker
 
   private
 
+  def home_team_pop_object
+    Stat.find_or_create_by(
+      game: @game,
+      season: @game.season,
+      name: "pop",
+      team: @home_team
+    )
+  end
+
+  def away_team_pop_object
+    Stat.find_or_create_by(
+      game: @game,
+      season: @game.season,
+      name: "pop",
+      team: @away_team
+    )
+  end
+
+  def home_team_pdp_object
+    Stat.find_or_create_by(
+      game: @game,
+      season: @game.season,
+      name: "pdp",
+      team: @away_team
+    )
+  end
+
+  def away_team_pdp_object
+    Stat.find_or_create_by(
+      game: @game,
+      season: @game.season,
+      name: "pdp",
+      team: @home_team,
+    )
+  end
+
   def set_pop_and_pdp
-    home_team_pop = @game.home_team_score.to_f / @game.home_team_drives.to_f
-    away_team_pop = @game.away_team_score.to_f / @game.away_team_drives.to_f
+    home_team_pop_value = @game.home_team_score.to_f / @game.home_team_drives.to_f
+    away_team_pop_value = @game.away_team_score.to_f / @game.away_team_drives.to_f
 
-    # Points per Offensive Possession
-    # How many points did you get on average every time you had the ball?
-    Stat.find_or_create_by(
-      game: @game,
-      season: @game.season,
-      name: "pop",
-      team: @home_team,
-      value: home_team_pop
-    )
+    home_team_pop = home_team_pop_object
+    home_team_pop.value = home_team_pop_value
+    home_team_pop.save
 
-    Stat.find_or_create_by(
-      game: @game,
-      season: @game.season,
-      name: "pop",
-      team: @away_team,
-      value: away_team_pop
-    )
+    away_team_pop = away_team_pop_object
+    away_team_pop.value = away_team_pop_value
+    away_team_pop.save
 
-    # Points per Defensive Possession
-    # How many points did you allow on average every time your opponent had the ball?
-    # Same as opponent's POP
-    Stat.find_or_create_by(
-      game: @game,
-      season: @game.season,
-      name: "pdp",
-      team: @away_team,
-      value: home_team_pop
-    )
+    home_team_pdp = home_team_pdp_object
+    home_team_pdp.value = away_team_pop_value
+    home_team_pdp.save
 
-    Stat.find_or_create_by(
-      game: @game,
-      season: @game.season,
-      name: "pdp",
-      team: @home_team,
-      value: away_team_pop
-    )
+    away_team_pdp = away_team_pdp_object
+    away_team_pdp.value = home_team_pop_value
+    away_team_pdp.save
   end
 end
