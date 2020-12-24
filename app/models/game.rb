ffbbe1a4506a2258e99229181efd56cb53f7419e
Pete Hanner <petehanner@gmail.com>
2020-12-23 21:45:08 -0500

    Add team access methods to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,12 @@ class Game < ApplicationRecord
   has_many :teams, -> { distinct }, through: :stats
 
   validates_presence_of :api_ref
+
+  def home_team
+    Team.find_by(name: home_team_name)
+  end
+
+  def away_team
+    Team.find_by(name: away_team_name)
+  end
 end
