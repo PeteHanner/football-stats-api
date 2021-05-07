96a4a32f592cbf98fc23c38dd0bc365770450c8b
Pete Hanner <petehanner@gmail.com>
2021-05-06 21:21:53 -0400

    Add route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :teams, only: [:show]
+
   get "/seasons/:season", to: "teams#index", as: "season"
   get "/seasons/", to: "teams#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
