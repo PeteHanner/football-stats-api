c1d21c8fd730e2917274a6c4ac92c9978e5eb155
Pete Hanner <petehanner@gmail.com>
2021-05-06 20:59:52 -0400

    Add urlnicks to teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_210859) do
+ActiveRecord::Schema.define(version: 2021_05_07_005756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_12_19_210859) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "urlnick"
   end
 
   add_foreign_key "stats", "games"
