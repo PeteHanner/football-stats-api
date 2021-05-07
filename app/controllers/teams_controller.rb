b1a660b7548803b99e3b8db37e4b21a3593859e8
Pete Hanner <petehanner@gmail.com>
2021-05-06 20:49:30 -0400

    Adjust controller routing for front end work

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,12 @@
 class TeamsController < ApplicationController
-  def show
+  def index
     season = params[:season].to_i
-    return redirect_to season_path(Stat.current_season) if bad_params?
-
-    team_stats = Team.all_with_games_in_season(season)
-
-    render json: team_stats, season: season
+    if bad_params?
+      render json: []
+    else
+      team_stats = Team.all_with_games_in_season(season)
+      render json: team_stats, season: season
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/seasons/:season", to: "teams#show", as: "season"
-  get "/seasons/", to: "teams#show"
+  get "/seasons/:season", to: "teams#index", as: "season"
+  get "/seasons/", to: "teams#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
