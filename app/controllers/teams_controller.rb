d611ae735ccd29ca8c8e1fc466ded11d009f83de
Pete Hanner <petehanner@gmail.com>
2021-05-09 11:00:54 -0400

    Change private method to more accurate name

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamsController < ApplicationController
   def index
     season = params[:season].to_i
-    if bad_params?
+    if no_season_games?
       render json: []
     else
       team_stats = Team.all_with_games_in_season(season)
@@ -17,7 +17,7 @@ class TeamsController < ApplicationController
 
   private
 
-  def bad_params?
+  def no_season_games?
     params[:season].blank? || Game.find_by(season: params[:season]).blank?
   end
 end
